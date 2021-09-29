ac921073fa0eab5da41a0b702a00cc68e8fe813b
irikras <irikras@mail.ru>
2021-09-29 15:09:08 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTestV1.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+    }
+}
